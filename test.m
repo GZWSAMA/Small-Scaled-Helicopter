7304de62d3bcb6ab82d4c3a12637250f6a4a1954
Geng Zheng <2243873432@qq.com>
2024-10-09 17:50:14 +0800

    remove irrelevant files

--- test.m
+++
@@ -1,7 +0,0 @@
-close all
-
-figure(1)
-plot3(out.simout.Data(1,:),out.simout.Data(2,:),out.simout.Data(3,:));
-hold on
-plot3(out.simout1.Data(1,:),out.simout1.Data(2,:),out.simout1.Data(3,:));
-zlim([-10,10])
